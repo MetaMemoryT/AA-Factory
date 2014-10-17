697a38bba03f279331c85f4cd7d5e349284b48db
Sean Usick <sean258@gmail.com>
2014-10-17 15:27:10 -0500

    remove jquery mobile

--- src/controller.js
+++ src/controller.js
@@ -2,68 +2,12 @@
 var s = {};
 
 var newCode = null;
-var qs = document.querySelector.bind(document);
-var qsA = document.querySelectorAll.bind(document);
 
 function saveAndLoad() {
   // eval user code into a function
-  var strCode = qs("#editor").innerHTML;
+  var strCode = $("#editor")[0].innerHTML;
   $.globalEval("var newCode = " + strCode);
   s.currentLevel.map.setAI(newCode);
-}
-
-function toggleLDesc() {
-  if (JSON.parse(localStorage.lDescVis)) {
-    localStorage.lDescVis = JSON.stringify(false);
-    setLDescVis(false);
-  } else {
-    localStorage.lDescVis = JSON.stringify(true);
-    setLDescVis(true);
-  }
-}
-
-function setLDescVis(bool) {
-  if (bool) {
-    for (var d of Array.prototype.slice.call(qsA('.lDesc')))
-      d.style.display = "none";
-    qs('#toggleLDesc').innerHTML = "Show Level Descriptions";
-  } else {
-    for (var d of Array.prototype.slice.call(qsA('.lDesc')))
-      d.style.display = "";
-    qs('#toggleLDesc').innerHTML = "Hide Level Descriptions";
-  }
-}
-
-
-var levels = [
-  Level1,
-  Level2,
-  Level3
-]
-
-function setLevel(levelI) {
-  localStorage.levelI = JSON.stringify(levelI);
-
-  var ide = qs('#ide');
-  ide.remove();
-  qs('core-animated-pages').selectIndex(levelI).appendChild(ide);
-
-  var levelcons = levels[levelI];
-  var level = new levelcons();
-  changeOrRestartLevel(level);
-  qs("#editor").innerHTML = level.answers[0].toString();
-  qs('#forward-button').disabled = false;
-  qs('#back-button').disabled = false;
-  qs('#chapterName').innerHTML = "L" + (levelI + 1) + ": " + level.name;
-  if (levelI == (levels.length - 1)) qs('#forward-button').disabled = true;
-  if (levelI == 0) qs('#back-button').disabled = true;
-}
-
-function changeLevel(num) {
-  var i = qs('core-animated-pages').selectedIndex;
-  var levelI = i + num; // zero-indexed level value
-
-  setLevel(levelI);
 }
 
 s.table = null;
@@ -86,13 +30,6 @@ function changeOrRestartLevel(level) {
   s.endlevelO.open(function(n, o) {
     changeState(false);
     $("#resume").prop("disabled", true);
-    var levelI = JSON.parse(localStorage.levelI);
-    if (s.currentLevel.endLevelState == EndLevelState.WIN) {
-      alert("You beat level " + (levelI + 1));
-    }
-    if (s.currentLevel.endLevelState == EndLevelState.LOSE) {
-      alert("You lost level " + (levelI + 1) + ". Error message: " + s.currentLevel.endMessage);
-    }
   });
 }
 
@@ -120,7 +57,7 @@ $("#step").click(function(ev) {
   stepRender();
 });
 $("#run").click(function(ev) {
-  changeOrRestartLevel(s.currentLevel);
+  changeOrRestartLevel(new Level1());
   changeState(true);
 });
 $("#saveLoad").click(function(ev) {
@@ -133,17 +70,4 @@ $("#resume").click(function(ev) {
   changeState(true);
 });
 
-window.addEventListener('polymer-ready', function(e) {
-  if (localStorage.levelI) {
-    setLevel(JSON.parse(localStorage.levelI));
-  } else {
-    setLevel(0);
-  }
-
-  if (localStorage.lDescVis) {
-    setLDescVis(JSON.parse(localStorage.lDescVis));
-  } else {
-    localStorage.lDescVis = JSON.stringify(true);
-    setLDescVis(true);
-  }
-});
+changeOrRestartLevel(new Level1());
