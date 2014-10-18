00143b59025bb312b07ba7a92fdf7714a7201731
Christopher Usick <christopher.usick@gmail.com>
2014-10-18 10:25:59 -0500

    working on level 3
    added Map.getNeighbors, Map.getValidNeigbors. Changed Directions class
    to have 8 directions. Added tests.js for running tests, currently part
    of index.html.
    Added lodash to bower Deps for array union.
    Got Duds moving on level3

--- src/model.js
+++ src/model.js
@@ -1,9 +1,27 @@
 // model should know nothing of the view or rendering, (no DOM interaction)
 var Directions = {
-  LEFT: 0,
-  UP: 1,
-  RIGHT: 2,
-  DOWN: 3
+  UPLEFT:0,
+  LEFT:1,
+  DOWNLEFT:2,
+  UP: 3,
+  CENTER: 4,
+  DOWN:5,
+  UPRIGHT: 6,
+  RIGHT: 7,
+  DOWNRIGHT:8,
+  
+}
+var Dirs = {
+  // shorthands
+  UL:Directions.UPLEFT,
+  U:Directions.UP,
+  UR:Directions.UPRIGHT,
+  L: Directions.LEFT,
+  R: Directions.RIGHT,
+  DL: Directions.DOWNLEFT,
+  D: Directions.DOWN,
+  DR: Directions.DOWNRIGHT,
+  C:Directions.CENTER
 }
 var EndLevelState = {
   WIN: "win",
@@ -37,6 +55,12 @@ function Map(x, y, robots, level) {
   this.robots.forEach(function(r) {
     self.tiles[r.x][r.y] = mapColor.key("robot");
   })
+  /**
+   * the default function for giving data to a robot
+   * note: the `this` keyword is set to the map inside this function when it is called
+   * @param  {Robot} robot a the specific robot to get data for
+   * @return {Array}       an array of args to pass to Robot.lambda
+   */
   this.getRobotData = function (robot) {
     return [robot.x, robot.y, this.tiles, robot.state];
   }
@@ -88,11 +112,37 @@ Map.prototype.step = function() {
   }
 }
 
+Map.prototype.getNeighbors = function(x,y) {
+   var minX = Math.max(0, x-1)
+       , maxX = Math.min(this.width - 1, x+1)
+       , minY = Math.max(0, y-1)
+       , maxY = Math.min(this.height - 1, y+1);
+  var neis = [];
+  for (var i = x-1; i <= x+1; i++) {
+    for (var j = y-1; j <= y+1; j++) {
+      // need to push null if the tiles is out of bounds
+      if (i < minX || i > maxX || j < minY || j > maxY) neis.push(null);
+      else neis.push(this.tiles[i][j]);
+    }
+  }
+  return neis;
+}
+
+Map.prototype.getValidDirections = function(x,y) {
+  var dirs = [];
+  var neis = this.getNeighbors(x,y);
+  for (var dir in Directions){
+    // 0 evaluates to false!!!
+    if (neis[Directions[dir]] != null) dirs.push(Directions[dir]);
+  }
+  return dirs;
+};
+
 // things all levels have
-function Level(size) {
+function Level(size, map) {
   this.endMessage = null;
   this.endLevelState = null;
-  this.map = new Map(size, size, [new Robot(0, 0)], this);
+  this.map = map || new Map(size, size, [new Robot(0, 0)], this);
   this.turn = 0;
 }
 Level.prototype.step = function() {
@@ -160,7 +210,12 @@ Level2.prototype.testVictory = function() {
  * seek and destroy
  */
 function Level3() {
-  Level.call(this, 5);
+  var duds = [];
+  for (var i = 0; i < 2; i++){
+    duds.push(new Dud(Math.floor(Math.random()*6), Math.floor(Math.random()*6)));
+  }
+  var map = new Map(7,7, [new Robot(0,0)].concat(duds) , this);
+  Level.call(this, 5, map);
   this.name = "Seek and Destroy";
   this.answers = [
     function(x, y, tiles, info) {
@@ -174,8 +229,16 @@ function Level3() {
     return [robot.x, robot.y, this.getNeighbors(robot.x, robot.y)
           , this.getValidDirections(robot.x, robot.y), robot.state]
   }
+
+  // change the default setAI behavior
+  this.map.setAI = function (fn) {
+    this.robots[0].lambda = fn;
+  }
 }
 Level3.prototype = Object.create(Level.prototype);
+Level3.prototype.testVictory = function() {
+  return false;
+}
 
 function Dud (xVal, yVal) {
   Robot.call(this, xVal, yVal);
