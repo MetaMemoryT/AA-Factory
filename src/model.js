1f2d86350705f56bb27356e2f56353a1c1cf2f17
Christopher Usick <christopher.usick@gmail.com>
2014-10-19 19:40:38 -0500

    Added victory for level 3
    added custom logic callbacks for running additional logic in
    Map.prototype.step.
    made Level3.prototype.testVictory()

--- src/model.js
+++ src/model.js
@@ -53,15 +53,19 @@ function Map(x, y, robots, level) {
   this.width = x;
   this.height = y;
   this.tiles = [];
+  // additional logic callbacks executed in Map.step
+  this.stepLogicCbs = [];
   for (var i = 0; i < this.width; i++) {
     this.tiles[i] = new Array(this.height);
     for (var j = 0; j < this.tiles[i].length; j++) {
       this.tiles[i][j] = mapColor.key("empty");
     }
   }
+
+
   var self = this;
   this.robots.forEach(function(r) {
-    self.tiles[r.x][r.y] = mapColor.key("robot");
+    self.tiles[r.x][r.y] = self.colorRobot(r);
   })
   /**
    * the default function for giving data to a robot
@@ -72,6 +76,17 @@ function Map(x, y, robots, level) {
   this.getRobotData = function (robot) {
     return [robot.x, robot.y, this.tiles, robot.state];
   }
+
+  
+}
+
+/**
+ * Default method for giving a color to a tile
+ * @param  {Robot} robot 
+ * @return {number}       Color to be returned
+ */
+Map.prototype.colorRobot = function (robot) {
+  return mapColor.key("robot")
 }
 Map.prototype.makeErrorMessage = function(x, y, dir) {
   return "Robot at (" + x + "," + y + ") went out of bounds heading " + dir + ".";
@@ -80,45 +95,6 @@ Map.prototype.setAI = function(fn) {
   for (var r of this.robots)
     r.lambda = fn;
 }
-
-// Map.prototype.step = function() {
-//   for (var i = 0; i < this.robots.length; i++) {
-//     var r = this.robots[i];
-//     var dir = r.lambda.apply(null, this.getRobotData.call(this, r));
-//     this.tiles[r.x][r.y] = mapColor.key("empty");
-//     switch (dir) {
-//       case Directions.LEFT:
-//         if (r.x <= 0) {
-//           this.level.endMessage = this.makeErrorMessage(r.x, r.y, "left");
-//           this.level.endLevelState = EndLevelState.LOSE;
-//         } else
-//           r.x -= 1;
-//         break;
-//       case Directions.RIGHT:
-//         if (r.x + 1 > (this.width - 1)) {
-//           this.level.endMessage = this.makeErrorMessage(r.x, r.y, "right");
-//           this.level.endLevelState = EndLevelState.LOSE;
-//         } else
-//           r.x += 1;
-//         break;
-//       case Directions.UP:
-//         if (r.y - 1 < 0) {
-//           this.level.endLevelState = EndLevelState.LOSE;
-//           this.level.endMessage = this.makeErrorMessage(r.x, r.y, "up");
-//         } else
-//           r.y -= 1;
-//         break;
-//       case Directions.DOWN:
-//         if (r.y + 1 > (this.height - 1)) {
-//           this.level.endMessage = this.makeErrorMessage(r.x, r.y, "down");
-//           this.level.endLevelState = EndLevelState.LOSE;
-//         } else
-//           r.y += 1;
-//         break;
-//     }
-//     this.tiles[r.x][r.y] = mapColor.key("robot");
-//   }
-// }
 
 Map.prototype.step = function() {
   for (var i = 0; i < this.robots.length; i++) {
@@ -131,7 +107,10 @@ Map.prototype.step = function() {
     })
     if (validDir) {
       this.transform(r, dir)
-      this.tiles[r.x][r.y] = mapColor.key("robot");
+      for (var fn of this.stepLogicCbs) {
+        fn.call(this, r, dir);
+      }
+      this.tiles[r.x][r.y] = this.colorRobot.call(this, r);
     } else {
       this.level.endMessage = this.makeErrorMessage(r.x, r.y, getDirName(dir));
       this.level.endLevelState = EndLevelState.LOSE;
@@ -274,14 +253,26 @@ Level2.prototype.testVictory = function() {
 }
 
 /**
+ * Special Map for level 3
+ */
+function L3Map (w, h, robots, level) {
+  Map.call(this, w, h, robots, level)
+}
+L3Map.prototype = Object.create(Map.prototype);
+L3Map.prototype.colorRobot = function(robot) {
+  if(robot instanceof Dud) return mapColor.key("target");
+  else return mapColor.key("robot");
+}
+/**
  * seek and destroy
  */
 function Level3() {
   var duds = [];
+  // add duds
   for (var i = 0; i < 2; i++){
     duds.push(new Dud(Math.floor(Math.random()*6), Math.floor(Math.random()*6)));
   }
-  var map = new Map(7,7, [new Robot(0,0)].concat(duds) , this);
+  var map = new L3Map(7,7, [new Robot(0,0)].concat(duds) , this);
   Level.call(this, 5, map);
   this.name = "Seek and Destroy";
   this.answers = [
@@ -301,10 +292,25 @@ function Level3() {
   this.map.setAI = function (fn) {
     this.robots[0].lambda = fn;
   }
+
+  // add functionality to remove a dud if the robot moves on it
+  this.map.stepLogicCbs.push(function(r, dir){
+    // this is set to the map
+    if (this.tiles[r.x][r.y] == 2) {
+      var dudToDestroy = [];
+      for (var d of this.robots.slice(1, this.robots.length)){
+        if (d.x == r.x && d.y == r.y) {
+          // if a dud is at the same location of the robot, kill it
+          dudToDestroy.push(d);
+        }
+      }
+      this.robots = _.difference(this.robots, dudToDestroy);
+    }
+  })
 }
 Level3.prototype = Object.create(Level.prototype);
 Level3.prototype.testVictory = function() {
-  return false;
+  return (this.map.robots.length == 1);
 }
 
 function Dud (xVal, yVal) {
